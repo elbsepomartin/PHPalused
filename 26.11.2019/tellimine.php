d42574993afb35db63492875608fd4d21e506db8
mart1nu <sepo-martin.elb@khk.ee>
2019-11-26 14:19:14 +0200

    php

--- 26.11.2019/tellimine.php
+++ 26.11.2019/tellimine.php
@@ -10,3 +10,6 @@ $toode3 = $_GET['t3'];
 echo 'Toode 1: '.$toode1.'tk<br>';
 echo 'Toode 2: '.$toode2.'tk<br>';
 echo 'Toode 3: '.$toode3.'tk<br>';
+echo 'Tooted Kokku:'.($toode1+$toode2+$toode3).'<br>';
+echo 'Hind Kokku:'.($toode1*5+$toode2*3+$toode3*2).'€';
+?>
---
+++ 26.11.2019/time/yl8.php
@@ -0,0 +1,10 @@
+<?php
+
+// Kuva kuupäev ja kellaaeg formaadis 20.03.2013 12:31
+// Kuva tänane eestikeelne nädalapäev, N: kolmapäev
+// Kuva eestikeelne kuupäev koos nädalapäevaga. Näiteks: 23.veebruar 2013 laupäev
+// Leia mitu päeva on jäänud järgmise jaanipäevani. Näiteks: 2014 aasta jaanipäevani on jäänud 236 päeva!
+// Minu sünnipäev on 06.11.1980! Leia kumb on meist vanem. Kuva mõlema sünnikuupäevad ning vahe aastates.
+// Maailmalõpp saabub 29.02.2016! Las PHP otsustab, kas see on võimalik.
+// Leia, kas sul on järgmine aasta juubel?
+// Koosta kood, mis tervitab sind vastavalt ajale. N: 8:00+ “Tere hommikust!”, 13:00+ “Tere päevast!” ja 17:00+ “Tere õhtust!”

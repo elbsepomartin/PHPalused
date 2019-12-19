24ae4e9db20aba7f89fda1cfecc61de6e679a36c
mart1nu <sepo-martin.elb@khk.ee>
2019-12-19 11:08:22 +0200

    php

---
+++ php/session/login.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'config.php';
+require_once 'db_fnk.php';
+require_once 'output.php';
+
+// näita sisselogimisvormi
+loginVorm();
+
+// kontrollime vormi poolt andmeid
+if(!empty($_POST['nimi']) and !empty($_POST['parool'])){
+    $nimi = $_POST['nimi'];
+    $parool = $_POST['parool'];
+
+    // pärime antud nimega kasutaja andmed andmebaasist
+    $sql = 'SELECT * FROM kasutajad WHERE nimi="'.$nimi.'" AND parool="'.md5($parool).'"';
+    $result = getData($sql, $ikt);
+    echo '<pre>';
+    print_r($result);
+}
